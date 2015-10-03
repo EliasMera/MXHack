9d75b4dfe09a444a73058223ddcf57008d9cecca
JJLionHeart <jj.lopezjaimez@gmail.com>
2015-10-03 17:25:52 -0500

    added code for server

--- Main Server/user.java
+++ Main Server/user.java
@@ -1,6 +1,14 @@
 public class User{
-	public static void main(String[] args){
-		System.out.println("Hello World");
+	private String id; //The id is the phone number
+	private String name;
+	public void setID(String i){
+		this.id = i;
 	}
-	
+	public String getID(){
+		return this.id;
+	}
+	public User(String i, String n){
+		this.id = i;
+		this.name = n;
+	}
 }
