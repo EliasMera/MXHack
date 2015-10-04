f6efea203a4bf6cdf8b6df32b8d784908204947c
JJLionHeart <jj.lopezjaimez@gmail.com>
2015-10-04 10:03:49 -0500

    primer prototipo funcional completo

--- Munch2/app/src/main/java/com/example/guillermo/munch2/InviteFriends.java
+++ Munch2/app/src/main/java/com/example/guillermo/munch2/InviteFriends.java
@@ -1,12 +1,18 @@
 package com.example.guillermo.munch2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InviteFriends extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar toolbar;
+    public void enviar(View v){
+        Intent it = new Intent(this,payment_method.class);
+        startActivity(it);
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
