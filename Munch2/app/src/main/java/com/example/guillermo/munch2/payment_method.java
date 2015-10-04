3cf5c883e6b1c9ea8c0dfbfbad632df8b04ada2d
EliasMera <elias_mera96@hotmail.com>
2015-10-04 10:48:36 -0500

    Munch

--- Munch2/app/src/main/java/com/example/guillermo/munch2/payment_method.java
+++ Munch2/app/src/main/java/com/example/guillermo/munch2/payment_method.java
@@ -1,25 +1,19 @@
 package com.example.guillermo.munch2;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class InviteFriends extends AppCompatActivity {
+public class payment_method extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar toolbar;
-    public void enviar(View v){
-        Intent it = new Intent(this,payment_method.class);
-        startActivity(it);
 
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_invite_friends);
-
+        setContentView(R.layout.activity_payment_method);
 
         toolbar= (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
     }
 }
